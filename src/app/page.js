a3036278750cb6d39ca2d33a8dd9dae6c311da8d
Fuad Hasan <fuadhasan3503@gmail.com>
2025-08-26 00:01:23 +0600

    create a review section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from '@/components/Hero';
 import HighlightProducts from '@/components/HighlightProducts';
+import Review from '@/components/Review';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <HighlightProducts />
+      <Review/>
     </>
   );
 }
